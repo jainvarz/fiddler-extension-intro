17e708acad59c140569ff20d4676bb56cfd2ffa2
Varun Jain <varjain@SEA.CORP.EXPECN.com>
2017-12-10 15:44:44 +0530

    Initial commit for fiddler extension intro

---
+++ MyFiddlerExtension/Extension.cs
@@ -0,0 +1,37 @@
+﻿using Fiddler;
+
+[assembly: RequiredVersion("2.2.8.6")]
+namespace MyFiddlerExtension
+{
+    public class Extension : IAutoTamper
+    {
+        public void AutoTamperRequestAfter(Session oSession)
+        {
+        }
+
+        public void AutoTamperRequestBefore(Session oSession)
+        {
+            oSession.oRequest["Test-Fiddler-Extension"] = "MyTestExtension";
+        }
+
+        public void AutoTamperResponseAfter(Session oSession)
+        {
+        }
+
+        public void AutoTamperResponseBefore(Session oSession)
+        {
+        }
+
+        public void OnBeforeReturningError(Session oSession)
+        {
+        }
+
+        public void OnBeforeUnload()
+        {
+        }
+
+        public void OnLoad()
+        {
+        }
+    }
+}
